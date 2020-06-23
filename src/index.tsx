ca337040b21908d1572c03134ba4e36faf0b87d6
Maksim Ramanenkau <maksim.ramanenkau@gmail.com>
2020-06-23 17:36:37 +0300

    Add antd

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,51 @@
 import * as React from 'react';
 import { render } from 'react-dom';
+import { Layout, Input, Typography, Space, Carousel } from 'antd';
+import 'antd/dist/antd.css';
 
-const App = () => {
-    return (
-        <React.Fragment>
-            <div className="container">
-                <h1>Content</h1>
-            </div>
-            <hr/>
-            <div className="logo"></div>
-        </React.Fragment>
-    );
-}
+const { Header, Footer, Sider, Content } = Layout;
+const { Search } = Input;
+const { Title } = Typography;
 
-render(<App />, document.getElementById('app'))
+render(
+    <>
+        <Layout>
+            <Header style={{ color: 'blue'}}>
+                <Space>
+                    <Title style={{ color: 'red'}} level={3}>OKEA</Title>
+                    <Search
+                        placeholder="input search text"
+                        onSearch={value => console.log(value)}
+                        style={{ width: 200 }}
+                    />
+                </Space>
+            </Header>
+            <Layout>
+                <Sider>
+                    Categories
+                </Sider>
+                <Content>
+                    <Carousel autoplay>
+                        <div>
+                            <h3>1</h3>
+                        </div>
+                        <div>
+                            <h3>2</h3>
+                        </div>
+                        <div>
+                            <h3>3</h3>
+                        </div>
+                        <div>
+                            <h3>4</h3>
+                        </div>
+                    </Carousel>
+                </Content>
+            </Layout>
+            <Content>
+                Content
+            </Content>
+            <Footer>Footer</Footer>
+        </Layout>
+    </>,
+    document.getElementById('app'),
+);
